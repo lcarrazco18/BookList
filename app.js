17696da84993dfa1d7b61e3aa12ec5e03779fc7f
Luis Carrazco <luiscarrazco@gmail.com>
2019-06-13 14:57:28 -0600

    show success

--- app.js
+++ app.js
@@ -80,6 +80,9 @@ document.getElementById('book-form').addEventListener('submit', function (e) {
 
     // Add book to list
     ui.addBookToList(book);
+    
+    // Show success
+    ui.showAlert('Book Added!', 'success');
 
     // Clear fields
     ui.clearFields();
