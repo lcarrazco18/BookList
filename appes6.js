1f8bfa46a9df13eb50a77a3f7c2c2dbcba1a58d3
Luis Carrazco <luiscarrazco@gmail.com>
2019-06-18 17:32:56 -0600

    Add local strg to project get,display,add, remove

--- appes6.js
+++ appes6.js
@@ -52,6 +52,53 @@ class UI {
     }
 }
 
+// Local Storage Class
+class Store {
+    static getBooks() {
+        let books;
+        if (localStorage.getItem('books') === null) {
+            books = [];
+        } else {
+            books = JSON.parse(localStorage.getItem('books'));
+        }
+
+        return books;
+    }
+    static displayBooks() {
+        const books = Store.getBooks();
+
+        books.forEach(function (book) {
+            const ui = new UI;
+
+            //Add book to UI
+            ui.addBookToList(book);
+        });
+    }
+
+    static addBook(book) {
+        const books = Store.getBooks();
+
+        books.push(book);
+
+        localStorage.setItem('books', JSON.stringify(books));
+    }
+
+    static removeBook(isbn) {
+        const books = Store.getBooks();
+
+        books.forEach(function (book, index) {
+            if (book.isbn === isbn) {
+                books.splice(index, 1);
+            }
+        });
+
+        localStorage.setItem('books', JSON.stringify(books));
+    }
+}
+
+// DOM Load Event
+document.addEventListener('DOMContentLoaded', Store.displayBooks);
+
 // Event Listener for add book
 document.getElementById('book-form').addEventListener('submit', function (e) {
     // Get form values
@@ -65,6 +112,8 @@ document.getElementById('book-form').addEventListener('submit', function (e) {
     // Instantiate UI
     const ui = new UI();
 
+    console.log(ui);
+
     // Validate
     if (title === '' || author === '' || isbn === '') {
         // Error alert
@@ -72,6 +121,9 @@ document.getElementById('book-form').addEventListener('submit', function (e) {
     } else {
         // Add book to list
         ui.addBookToList(book);
+
+        // Add to LS
+        Store.addBook(book);
 
         // Show success
         ui.showAlert('Book Added!', 'success');
@@ -88,9 +140,15 @@ document.getElementById('book-list').addEventListener('click', function (e) {
 
     // Instantiate UI
     const ui = new UI();
+
+    // Delete book
     ui.deleteBook(e.target);
 
+    // Remove from LS
+    Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
+
     // Show message
+    ui.showAlert('Book Removed!', 'success');
 
     e.preventDefault();
 });
